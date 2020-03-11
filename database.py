218550accc2c593fd394caf4b72c5773f958c792
root <root@2onv.l.time4vps.cloud>
2020-03-11 19:26:58 +0200

    Fixed database.py

--- database.py
+++ database.py
@@ -4,14 +4,13 @@ import hashlib
 
 class database():
 	def __init__(self, login, password):
-		server = mysql.connector.connect(host="localhost", user=login, password=password)
-		server.cursor().execute("CREATE DATABASE IF NOT EXISTS database")
-		self.cursor = mysql.connector.connect(host="localhost", user=login, password=password, database="database").cursor()
+		self.con = mysql.connector.connect(host="localhost", user=login, password=password, database="knowledge")
+		self.cursor = self.con.cursor()
 		self.cursor.execute('CREATE TABLE IF NOT EXISTS spells (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), public BOOLEAN, obvious BOOLEAN, required_const REAL, mana_cost INT, descripion_file VARCHAR(100), school VARCHAR(8))')
-		self.cursor.execute('CREATE TABLE IF NOT EXISTS spell_reqs (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), spell INT FOREIGN KEY REFERENCES spells(id), dependence VARCHAR(3))') #dependence is mul(*), div, msq(*x^2), dsq or exp
-		self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCRMENT PRIMARY KEY, name VARCHAR(50), learning_const REAL, school VARCHAR(8), biography_file VARCHAR(50), pass_hash CHAR(128), status INT)') #пароли должны хешироваться SHA-512; статус - 3 - Admin, 2 - Master, 1 - User, -1 - Banned
+		self.cursor.execute('CREATE TABLE IF NOT EXISTS spell_reqs (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), spell INT, FOREIGN KEY (spell) REFERENCES spells (id), dependence VARCHAR(3))') #dependence is mul(*), div, msq(*x^2), dsq or exp
+		self.cursor.execute('CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), learning_const REAL, school VARCHAR(8), biography_file VARCHAR(50), pass_hash CHAR(128), status INT)') #пароли должны хешироваться SHA-512; статус - 3 - Admin, 2 - Master, 1 - User, -1 - Banned
 		self.cursor.execute('CREATE TABLE IF NOT EXISTS beasts (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(50), danger_class INT, description_file VARCHAR(50))')
-		self.cursor.execute('CREATE TABLE IF NOT EXISTS spells_knowledge(user_id INT FOREIGN KEY REFERENCES users(id), spell_id INT FOREIGN KEY REFERENCES spells(id))')
+		self.cursor.execute('CREATE TABLE IF NOT EXISTS spells_knowledge(user_id INT, spell_id INT, FOREIGN KEY (user_id) REFERENCES users (id), FOREIGN KEY(spell_id) REFERENCES spells (id))')
 	
 	def add_spell(self, spell_params, spell_variables): #Первое - кортеж (название, публичность, очевидность(=способность понять по описанию каста, что кастуется), требования по константе обученности, расход маны, адрес файла с описанием, школа)
 		insert_request = 'INSERT INTO spells (name, public, obvious, required_const, mana_cost, description_file, school) VALUES (%s, %s, %s, %s, %s, %s, %s)'
@@ -51,7 +50,7 @@ class database():
 		request = 'UPDATE users SET status = %s WHERE id = %s'
 		self.cursor.execute(request, (user_id, status))
 	
-	
+database = database('knowledge', 'LainisOmniscient')	
 	
 	
 		
