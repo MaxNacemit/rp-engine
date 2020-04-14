12cf9371595f33603a5f084141be23812da98082
shar3nda <vyavya2003@yandex.ru>
2020-04-14 16:27:01 +0300

    hopefully fixed page refresh

--- static/js/spell.js
+++ static/js/spell.js
@@ -1,5 +1,4 @@
 function addParameter() {
-    // TODO no page refresh on text field addition
     let param_name = prompt("Что за параметр?", "дальность");
     let form = document.getElementById("inputs");
     let dependency_selector = param_name + ': <select name="' + param_name + '">' +
